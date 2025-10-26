20a2b4d6a5f32a98147ebade7909a3448f75ac10
Facundo Vattino <facuvatti@gmail.com>
2025-10-26 16:41:39 -0300

    Acorté el nombre de los meses y agregue un contador de faltas por cada mes

--- history.js
+++ history.js
@@ -133,19 +133,32 @@ async function studentGrid() {
         let body = document.querySelector("body");
         let div = document.createElement("div");
         div.id = "byStudent";
+        div.style.overflowX = "auto";
         let table = document.createElement("table");
+        table.style.width = "fit-content";
+        table.style.border = "1px solid black";
+        table.style.tableLayout = "fixed";
         let thead = document.createElement("thead");
         // Agregando las columnas (mes y numero de cada dia)
-        for(let i=0;i<32;i++) {
+        for(let i=0;i<33;i++) {
             let th = document.createElement("th");
             th.textContent = i;
+            th.style.textAlign = "center";
+            if(i == 32) th.textContent = "Faltas";
             if(i == 0) th.textContent = "Mes";
+            if(![0,32].includes(i)){
+                th.style.width= "2ch";
+            } else {
+                th.style.width= "7ch";
+            }
+    
             thead.append(th);
         }
         table.append(thead);
         div.append(table);
         body.insertBefore(div,document.querySelector("script"));
     }
+    if(document.querySelector("#ausents")) document.querySelector("#ausents").remove();
 }
 async function asistanceByStudent() {
     reset();
@@ -161,27 +174,44 @@ async function asistanceByStudent() {
     let tbody = document.createElement("tbody");
     tbody.innerHTML = "";
     let attendances = await httpRequest("student/asistances/"+studentId,"GET")
-    console.log(attendances);
     const Year = new Date().toISOString().split('T')[0].split("-")[0];
+    let yearAusents = 0;
     // Agregando las filas ( y la presencia del alumno en cada dia)
-    for(let month of ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]) {
+    for(let month of ["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"]) {
         // Nombre del mes
         let tr = document.createElement("tr");
         let td = document.createElement("td");
+        let ausents = 0;
         td.textContent = month;
         tr.append(td);
-        for(let day=1;day<=32;day++) {
+        for(let day=1;day<=31;day++) {
             // Asistencia del alumno por dia
             let td = document.createElement("td");
+            td.style.textAlign = "center";
             let presence = attendances.find(attendance => {if(attendance["date"].split(" ")[0] == Year+"-"+m+"-"+day) {td.textContent = attendance["presence"];return attendance;} else return undefined;});
-            if(presence) {td.textContent = presence["presence"];}
+            if(presence) {
+                td.textContent = presence["presence"];
+                if(presence["presence"] == "A") ausents++;
+                if(presence["presence"] in ["T","RA"]) ausents = ausents + 0.5;
+            }
             else {td.textContent = "";}
             tr.append(td);
         }
+        yearAusents += ausents;
+        td = document.createElement("td");
+        td.textContent = ausents;
+        td.style.textAlign = "center";
+        tr.append(td);
         tbody.append(tr);
         m++;
     }
     table.append(tbody);
+    let h1 = document.createElement("h1");
+    h1.id = "ausents";
+    if(yearAusents == 0) h1.textContent = "¡No tiene faltas!";
+    else h1.textContent = "Faltas: "+ yearAusents;
+    if(yearAusents > 20) h1.style.color = "red";
+    document.querySelector("body").append(h1);
 
 }
 async function showAsistances(by) {
